0d1936c453cff0c92b21a633d2efa6c88867f039
RECV00 <corralesreychell@gmail.com>
2025-04-01 13:38:38 -0600

    Creacion de la carpeta del proyecto
    Creacion base de la carpeta general

---
+++ ProjectoPasoPerezoso/src/main/java/cr/ac/una/perezoso/PerezosoApplication.java
@@ -0,0 +1,13 @@
+package cr.ac.una.perezoso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerezosoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerezosoApplication.class, args);
+	}
+
+}
---
+++ ProjectoPasoPerezoso/src/test/java/cr/ac/una/perezoso/PerezosoApplicationTests.java
@@ -0,0 +1,13 @@
+package cr.ac.una.perezoso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerezosoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
