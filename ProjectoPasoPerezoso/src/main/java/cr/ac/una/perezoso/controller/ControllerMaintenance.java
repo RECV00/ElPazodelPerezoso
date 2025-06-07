579ac619ad4ffede0883970228a8498131512ffb
AllisonCorrales <dayanacorrales6@gmail.com>
2025-06-07 17:13:39 -0600

    mm

--- ProjectoPasoPerezoso/src/main/java/cr/ac/una/perezoso/controller/ControllerMaintenance.java
+++ ProjectoPasoPerezoso/src/main/java/cr/ac/una/perezoso/controller/ControllerMaintenance.java
@@ -19,6 +19,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,7 +27,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 /**
  *
  * @author dayan
